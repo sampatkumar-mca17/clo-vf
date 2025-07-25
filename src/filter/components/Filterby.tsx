b07bad9abd41c37169aebebdd7b81771b5f19146
sampat <sampat.kumar@aquera.com>
2025-07-26 02:46:06 +0530

    more styling fixes

--- src/filter/components/Filterby.tsx
+++ src/filter/components/Filterby.tsx
@@ -19,47 +19,50 @@ function Filterby() {
         <div className="filterby">
             {deviceType=="mobile" && <FontAwesomeIcon icon={faBurger} onClick={()=>setIsMenuOpen(!isMenuOpen)}/>}
            <div className={deviceType=="mobile" ? "filter-reset-option filter-reset-option--mobile" : "filter-reset-option"}>
-            { isMenuOpen && 
+
                 <>
                     <div className={deviceType=="mobile" ? "pricing-options pricing-options--mobile" : "pricing-options"}>
 
                         <div className="pricing-options__text">
                             <span>Pricing Options</span>
                         </div>
-                        <div className="pricing-options__paid">
-                            <input 
-                            checked={filterOptions.paid} 
-                            data-testid="paid-checkbox"
-                            onChange={(e)=>dispatch(setFilterOptions({...filterOptions,paid:e.target.checked,priceRange:0}))} 
-                            type="checkbox" />
-                            <span>Paid</span>
-                        </div>
-                        <div className="pricing-options__free">
-                            <input 
-                            data-testid="free-checkbox"
-                            checked={filterOptions.free} 
-                            onChange={(e)=>dispatch(setFilterOptions({...filterOptions,free:e.target.checked}))} 
-                            type="checkbox" />
-                            <span>Free</span>
-                        </div>
-                        <div className="pricing-options__view-only">
-                            <input 
-                            data-testid="view-only-checkbox"
-                            checked={filterOptions.viewOnly} 
-                            onChange={(e)=>dispatch(setFilterOptions({...filterOptions,viewOnly:e.target.checked}))} type="checkbox" />
-                            <span>View Only</span>
-                        </div>
-                        <div className={`pricing-options__price-slider ${!filterOptions.paid ? 'disabled' : ''}`}>
-                            <span>Price Slider</span>
-                            <input data-testid="price-range" ref={priceRangeRef} className={`pricing-options__price-slider__input ${!filterOptions.paid ? 'disabled' : ''}`} disabled={!filterOptions.paid} type="range" min={0} max={100} value={filterOptions.priceRange} onChange={(e)=>dispatch(setFilterOptions({...filterOptions,priceRange:Number(e.target.value)}))}/>
-                            <span>{filterOptions.priceRange}$+</span>
-                        </div>
+                        { isMenuOpen &&
+                            <> 
+                            <div className="pricing-options__paid">
+                                <input 
+                                checked={filterOptions.paid} 
+                                data-testid="paid-checkbox"
+                                onChange={(e)=>dispatch(setFilterOptions({...filterOptions,paid:e.target.checked,priceRange:0}))} 
+                                type="checkbox" />
+                                <span>Paid</span>
+                            </div>
+                            <div className="pricing-options__free">
+                                <input 
+                                data-testid="free-checkbox"
+                                checked={filterOptions.free} 
+                                onChange={(e)=>dispatch(setFilterOptions({...filterOptions,free:e.target.checked}))} 
+                                type="checkbox" />
+                                <span>Free</span>
+                            </div>
+                            <div className="pricing-options__view-only">
+                                <input 
+                                data-testid="view-only-checkbox"
+                                checked={filterOptions.viewOnly} 
+                                onChange={(e)=>dispatch(setFilterOptions({...filterOptions,viewOnly:e.target.checked}))} type="checkbox" />
+                                <span>View Only</span>
+                            </div>
+                            <div className={`pricing-options__price-slider ${!filterOptions.paid ? 'disabled' : ''}`}>
+                                <span>Price Slider</span>
+                                <input data-testid="price-range" ref={priceRangeRef} className={`pricing-options__price-slider__input ${!filterOptions.paid ? 'disabled' : ''}`} disabled={!filterOptions.paid} type="range" min={0} max={100} value={filterOptions.priceRange} onChange={(e)=>dispatch(setFilterOptions({...filterOptions,priceRange:Number(e.target.value)}))}/>
+                                <span>{filterOptions.priceRange}$+</span>
+                            </div>
+                            </>
+                        }
                     </div>
-                    <div className="reset-option">
+                    {isMenuOpen && <div className="reset-option">
                         <span onClick={()=>dispatch(setFilterOptions({free:false,paid:false,viewOnly:false,priceRange:0}))}>Reset</span>
-                    </div>
+                    </div>}
                 </>
-            }
             </div>
             
         </div>
