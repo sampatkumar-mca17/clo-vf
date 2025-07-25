cf7fc6e0ea5d5c7d21fa188b45d2ae0e0f30d821
sampat <sampat.kumar@aquera.com>
2025-07-25 19:08:26 +0530

    nits

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -6,7 +6,7 @@ export const searchProducts = ():((filterOptions: FilterOptions, searchText: str
     return (filterOptions: FilterOptions, searchText: string, products: Product[], priceRange: number, sortValue: "relavance"|"higherPrice"|"lowerPrice") =>{
         const key = JSON.stringify({filterOptions, searchText, products, priceRange, sortValue});
         if(memo.has(key)){
-            console.warn('Search from cache');
+            console.log('Search from cache');
             return JSON.parse(memo.get(key)) as Product[];
         }
         const filteredProducts = filterAndSearch(products, filterOptions, searchText);
@@ -21,7 +21,7 @@ export const sortProducts = ():((sortValue: "relavance"|"higherPrice"|"lowerPric
     return (sortValue: "relavance"|"higherPrice"|"lowerPrice", products: Product[]):Product[] =>{
         const key = JSON.stringify({sortValue, products});
         if(memo.has(key)){
-            console.warn('Sort from cache');
+            console.log('Sort from cache');
             return JSON.parse(memo.get(key)) as Product[];
         }
         const productsCopy = [...products];
